af02efc65b78f76e29318e8fcbeeb4e20ee27057
Lucas Coquenão <cqn.lucas@gmail.com>
2023-12-27 15:45:18 -0300

    Implementada função de equipar item

---
+++ module/helpers/actions.js
@@ -0,0 +1,9 @@
+export function makeAction(event, actor){
+    console.log("CQN | Event")
+    console.log(event)
+
+}
+
+function _attackAction(){
+
+}
--- module/helpers/equipment.mjs
+++ module/helpers/equipment.mjs
@@ -5,6 +5,17 @@ export function equipGear(event, actor){
     const item = actor.items.get(li.data("itemId"));
 
     let updateData = {};
+
+    if(item.type == 'weapon'){
+        _equipWeapon(updateData, item);
+    } else {
+        _equipDefensive(updateData, item)
+    }
+
+    actor.update(updateData);
+}
+
+function _equipDefensive(updateData, item){
     let itemType = item.system.type
 
     updateData[`system.gear.${itemType}`] = item.id;
@@ -13,6 +24,8 @@ export function equipGear(event, actor){
     updateData[`system.mDefense.${itemType}Value`] = item.system.mDefense.value;
     updateData[`system.mDefense.${itemType}Attr`] = item.system.mDefense.attr;
     updateData[`system.initiative.${itemType}Value`] = item.system.initiative;
+}
 
-    actor.update(updateData);
+function _equipWeapon(updateData, item){
+    updateData[`system.gear.${item.type}`] = item.id;
 }
